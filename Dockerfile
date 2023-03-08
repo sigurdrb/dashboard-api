bf7bd81a69d12a388e9858def361ff444be3459d
sigurdrb <sigurdrb@gmail.com>
2023-03-08 20:58:23 +0100

    Testing Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM python:3
 COPY hello world.py /tmp/
 CMD ["python", "/tmp/hello world.py"
+RUN 
--- hello world.py
+++ hello world.py
@@ -1,8 +1,3 @@
-print("hello world!!")
-print("test update")
-print("fungerer dette egentlig?")
-print("gir dette mening?")
-
 x = 2
-y = 3
+y = 5
 print(x+y)
